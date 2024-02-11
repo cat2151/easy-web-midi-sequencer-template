76ac358d666efea6d13564637840d5cfbab7c2cc
cat2151 <fct_2004@mail.goo.ne.jp>
2024-02-11 18:01:10 +0900

    experimental10 iPad実機でのみ発生する問題が解決したので、logをコメントアウトし、関連divやspanを調整した

--- experimental10/knob/knob.js
+++ experimental10/knob/knob.js
@@ -1,32 +1,33 @@
 // 用途 : 簡易仮想Cutoffツマミのtest用
 // usage : parent.js / child.js の import "./knob.js"; 付近を参照ください
 const kb = { isTouch: false };
-const div1 = document.querySelector(`#div1`);
-const div2 = document.querySelector(`#div2`);
-// const elm = window;
-const elm = div1; // iPad実機のみで発生するバグの調査用
+const div = document.querySelector(`div`);    // index.htmlと結合しているので注意
+const span = document.querySelector(`span`);  // 〃
+// const elm = window; // 問題、windowだと、iPad実機のみ演奏開始後にeventが発生しなくなった。対策、windowのかわりにdivを指定することで解決した。
+const elm = div;
 
 elm.addEventListener("pointerdown", (ev) => {
-  console.log(`pointerdown`, ev);
+  // console.log(`pointerdown`, ev);
   if (kb.isTouch) return; // iPadやAndroidで意図せぬ発音処理をさせない用（タッチ後にタップでmouseイベントが発生してここに到達する）
   const x = Math.floor(ev.clientX);
   onmousedownOrTouchStart(x);
 });
 elm.addEventListener("pointermove", (ev) => {
-  console.log(`pointermove`, ev);
+  // console.log(`pointermove`, ev);
   if (kb.isTouch) return;
   const x = ev.clientX;
   onmousemoveOrTouchMove(x);
 });
 
 elm.addEventListener("touchstart", (ev) => {
-  console.log(`touchstart`, ev);
+  // console.log(`touchstart`, ev);
   kb.isTouch = true;
   const x = Math.floor(ev.changedTouches[0].clientX);
   onmousedownOrTouchStart(x);
-});
+  ev.preventDefault();
+}, { passive: false });
 elm.addEventListener("touchmove", (ev) => {
-  console.log(`touchmove`, ev);
+  // console.log(`touchmove`, ev);
   kb.isTouch = true;
   const x = Math.floor(ev.changedTouches[0].clientX);
   onmousemoveOrTouchMove(x);
@@ -63,8 +64,7 @@ function getKeyboardNoteNum(key) {
 // MIDI
 function cc74(v) {
   // console.log(`knob : cc74 : ${v}`);
-  div2.innerHTML = v; // iPad実機のみで発生するバグの調査用
-
+  span.innerHTML = v;
   kb.sendMidiMessage([[0xB0, 74, v]]);
 }
 
