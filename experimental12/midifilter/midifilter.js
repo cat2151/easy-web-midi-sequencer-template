3b611689efca15294a4a7f488d0dfe4b86148800
cat2151 <fct_2004@mail.goo.ne.jp>
2024-02-22 21:41:39 +0900

    experimental12 リファクタリングし、読みやすくした

--- experimental12/midifilter/midifilter.js
+++ experimental12/midifilter/midifilter.js
@@ -1,19 +1,22 @@
 function midifilter(data) {
     console.log(`midifilter : before : `, data);
-    const events = data[0];
-    const event0 = events[0];
 
-    // TODO XXX リファクタリングする
-    const event1 = new Uint8Array(event0);
-    event1[1] += 4;
-    events.push(event1);
-
-    const event2 = new Uint8Array(event0);
-    event2[1] += 7;
-    events.push(event2);
+    addShiftedNotes(data[0]);
 
     console.log(`midifilter : after : `, data);
     return data;
+}
+
+function addShiftedNotes(events) {
+    const orgNote = events[0];
+    events.push(getShiftedNote(orgNote, 4));
+    events.push(getShiftedNote(orgNote, 7));
+}
+
+function getShiftedNote(orgNoteEvent, semitone) {
+    const newNoteEvent = new Uint8Array(orgNoteEvent);
+    newNoteEvent[1] += semitone;
+    return newNoteEvent;
 }
 
 export { midifilter };
