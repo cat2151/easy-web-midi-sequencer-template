67e37231070d70da5bc1c16e48db2c6ba232fd97
cat2151 <fct_2004@mail.goo.ne.jp>
2024-03-11 22:19:45 +0900

    experimental16 samplerで1秒の波形を生成して鳴らすようにした

--- experimental16/sampler/sampler-child.js
+++ experimental16/sampler/sampler-child.js
@@ -6,8 +6,9 @@ const urlParams = rison2.parse(new URL(window.location.href).searchParams.get('q
 postmateMidi.registerChild(urlParams, null, null, null, null, null);
 const samplerParam1 = { urls: { A2: "handdrum-loop.mp3" }, baseUrl: "https://tonejs.github.io/audio/drum-samples/" };
 const samplerParam2 = { urls: { C5: "Analogsynth_octaves_highmid.mp3" }, baseUrl: "https://tonejs.github.io/audio/berklee/" };
+const samplerParam3 = null;
 initSampler(postmateMidi.ch[1-1], samplerParam1, /*volume=*/-1);
-initSampler(postmateMidi.ch[2-1], samplerParam2, /*volume=*/-9);
+initSampler(postmateMidi.ch[2-1], samplerParam3, /*volume=*/-9);
 
 postmateMidi.ui.visualizeCurrentSound();
 console.log(`postmate-midi child:`, postmateMidi);
--- experimental16/sampler/sampler.js
+++ experimental16/sampler/sampler.js
@@ -1,7 +1,17 @@
 // import * as Tone from 'tone'; // コメントアウトする。index.htmlでTone.jsをsrcする。そうしないとバンドラーを使わない別projectにおいてソースをそのまま利用できず不便だったので。
 
 function initSampler(s, samplerParam, volume) {
-  const sampler = new Tone.Sampler(samplerParam);
+  console.log(`sampler : init...`);
+  let sampler;
+
+  if (samplerParam) {
+    sampler = new Tone.Sampler(samplerParam);
+  } else {
+    sampler = new Tone.Sampler();
+    const noteNum = 60;
+    const wav = createWav(noteNum, 1/*sec*/);
+    sampler.add(noteNum, wav);
+  }
 
   sampler.toDestination();
   sampler.volume.value = volume;
@@ -17,6 +27,19 @@ function initSampler(s, samplerParam, volume) {
   function noteOff(noteNum, timestamp) {
     sampler.triggerRelease(Tone.Midi(noteNum).toFrequency(), timestamp);
   };
+
+  function createWav(noteNum, time) {
+    const sampleRate = Tone.context.sampleRate;
+    console.log(`sampler : sampling rate : ${sampleRate}`);
+    const freq = Tone.Midi(noteNum).toFrequency()
+    const twoPiFreqPerSampleRate = 2 * Math.PI * freq / sampleRate
+    let wav = new Float32Array(sampleRate * time);
+    for (let i = 0; i < wav.length; i++) {
+      wav[i] = 0.5 * Math.sin(i * twoPiFreqPerSampleRate);
+    }
+    console.log(`sampler : wav : `, wav);
+    return Tone.Buffer.fromArray(wav);
+  }
 }
 
 export { initSampler };
