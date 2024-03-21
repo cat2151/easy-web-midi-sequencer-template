e434d9ee1ddb89945f32f585566333c6fbb9f73a
cat2151 <fct_2004@mail.goo.ne.jp>
2024-03-21 22:10:25 +0900

    experimental18 synth

--- experimental18/synth/synth-child.js
+++ experimental18/synth/synth-child.js
@@ -1,5 +1,5 @@
 import { postmateMidi } from "../postmate-midi.js";
-import { gn } from "./generator.js";
+import { gn } from "./synth.js";
 if (!new URL(window.location.href).searchParams.get('query')) window.location.href = "../"; // dir階層直叩きした場合の混乱防止用
 const urlParams = rison2.parse(new URL(window.location.href).searchParams.get('query'));
 
